58d2a8e53c672931343736892756ea960c78a97b
Julio Lopez <ljuliom@gmail.com>
2015-10-05 17:30:03 -0500

    apply cache to assets and installing service worker

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,18 @@
 importScripts('serviceworker-cache-polyfill.js');
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/normalize.css',
+  '/styles/styles.css'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
